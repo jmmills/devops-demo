9b05a38869fab7f389e5f7eafb480990921b7603
Jason Mills <jason.mills@integratelecom.com>
2014-12-03 13:19:06 -0800

    version bump

--- MicroCMS.pl
+++ MicroCMS.pl
@@ -284,7 +284,7 @@ body {
 @@ index.html.ep
 %= include 'default';
 
-Version 1.1.0<br/><br/>
+Version 1.1.1<br/><br/>
 
 For demo please use the following details:<br>
 Email <b>admin@myproject.com</b>, Password: <b>admin</b><br>
